b863ee9504d8710217dc2f5cb8bf34f8ba5d3e32
Rita Rezende Borges de Lima <ritarezborges@gmail.com>
2022-05-31 17:42:05 -0300

    fixed hill climbing

--- src/hill_climbing.cpp
+++ src/hill_climbing.cpp
@@ -15,14 +15,12 @@ void HillClimbing::search(State init_state){
     while(lim--){
         State neighbor = State();
         int best_value = 0;
-        cout << current.get_key() << endl;
-        for(State next_state: init_state.get_possible_actions()){
+        for(State next_state: current.get_possible_actions()){
             if(next_state.value() > best_value){
                 best_value = next_state.value();
                 neighbor = next_state;
             }
         }    
-        cout << "n value: " << neighbor.value() << " c value: " << current.value() << endl;
         if(neighbor.value() < current.value())
             return ;
 
